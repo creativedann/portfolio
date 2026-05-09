7372270b8123fc7959eafeaa0c8e2106f1d86322
creativedann <jaanmallah16@gmail.com>
2026-05-09 09:12:05 -0700

    My portfolio website

---
+++ main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Project
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def projects(request):
+    all_projects = Project.objects.all()
+    return render(request, 'main/projects.html', {'projects': all_projects})
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+        # Baad mein database mein save karein ge!
+    return render(request, 'main/contact.html')
