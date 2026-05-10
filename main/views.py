372ca00f03a8681e541e3760a3505d4f3ea311ad
creativedann <jaanmallah16@gmail.com>
2026-05-10 08:21:47 -0700

    Fixed template path and settings

--- main/views.py
+++ main/views.py
@@ -2,18 +2,18 @@ from django.shortcuts import render
 from .models import Project
 
 def home(request):
-    return render(request, 'main/home.html')
+    return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'main/about.html')
+    return render(request, 'about.html')
 
 def projects(request):
     all_projects = Project.objects.all()
-    return render(request, 'main/projects.html', {'projects': all_projects})
+    return render(request, 'projects.html', {'projects': all_projects})
 def contact(request):
     if request.method == "POST":
         name = request.POST.get("name")
         email = request.POST.get("email")
         message = request.POST.get("message")
         # Baad mein database mein save karein ge!
-    return render(request, 'main/contact.html')
+    return render(request, 'contact.html')
